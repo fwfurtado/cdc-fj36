4b4376cc26800df9d741e991ce5726f994df70e7
Fernando <feh.wilinando@gmail.com>
2018-12-05 19:44:44 -0200

    Create buyer project

---
+++ buyer/src/main/java/br/com/caelum/cdc/buyer/BuyerBoot.java
@@ -0,0 +1,11 @@
+package br.com.caelum.cdc.buyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(BuyerBoot.class, args);
+    }
+}
